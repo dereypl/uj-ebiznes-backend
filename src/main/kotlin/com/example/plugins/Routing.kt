544207a72d466b9cd3b47a52a41829ee492d727a
dereypl <mpasich.pk@gmail.com>
2022-08-24 22:42:20 +0200

    Base route welcome message added.

--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -6,10 +6,14 @@ import io.ktor.server.application.*
 import io.ktor.server.routing.*
 import productsRoute
 import com.example.service.UserService
+import io.ktor.server.response.*
 import signIn
 
 fun Application.configureRouting(userService: UserService, simpleJWT: SimpleJWT) {
     routing {
+        get("/") {
+            call.respondText("Project: uj-ebiznes-backend is up and running!")
+        }
         productsRoute()
         categoriesRoute()
         orderRoutes()
